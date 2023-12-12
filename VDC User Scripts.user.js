1ac7c13aeae0b816715db0730d4dc5624a4d10b4
GabrielV <59255191+VDeltaGabriel@users.noreply.github.com>
2023-12-12 18:42:57 +0100

    Add files via upload

---
+++ VDC User Scripts.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         VDC User Scripts
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Launch user scripts from VDC!
+// @author       GabrielV
+// @match        https://developer.valvesoftware.com/*
+// @icon         https://developer.valvesoftware.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const currentUrl = window.location.href;
+    const match = currentUrl.match(/(User:)([a-zA-z]*)/);
+    if (match != null)
+    {
+        if (confirm('This user seems to have a custom user script! Do you want to load it? (REMEMBER: Just because it is on VDC **DOES NOT** mean it cannot contain potentially harmful/malicious code!)'))
+        {
+            const script = document.createElement('script');
+            script.src = `https://developer.valvesoftware.com/w/index.php?title=${match[0]}/common.js&action=raw&ctype=text/javascript`;
+            document.body.appendChild(script);
+        }
+    }
+})();
